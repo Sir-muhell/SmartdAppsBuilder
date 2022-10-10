e1a04db01117019cc5b240657c67911efbd8950e
Aholu Samuel <samuelaholu15@gmail.com>
2022-10-10 09:37:12 +0100

    Email

---
+++ submit.php
@@ -0,0 +1,21 @@
+<?php
+$email = $row['email'];
+$messsage = $_SESSION['message'];
+$to = "support@smartdappsbuilder.com";
+$from = $email;
+$headers = "From: " . $from . "\r\n";
+$headers .= "Reply-To: " . $from . "\r\n";
+$headers .= "Return-Path: " . $from . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=\"iso-8859-1\"\n";
+$headers .= "X-Mailer: PHP" . phpversion() . "\r\n";
+$headers .= "X-Priority: 1 (Highest)\n";
+$headers .= "X-MSMail-Priority: High\n";
+$headers .= "Importance: High\n";
+
+$subject = $subj;
+
+$body = "<p style='margin-left: 45px; margin-top: 34px; text-align: left; font-size: 17px;'>{$message} <br/>you are about to chage your password. </p>";
+$send = mail($to, $subject, $body, $headers);
+header("location:./")
+?>
